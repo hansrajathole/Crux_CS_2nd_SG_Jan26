42997c267f2b683cbf46faa553d4332d5d27bfdf
hansrajathole <hansrajathole1@gmail.com>
2026-01-19 17:35:37 +0530

    Add initial implementation of Demo and Variable classes with basic output functionality

---
+++ Lec2/Demo.java
@@ -0,0 +1,11 @@
+package Lec2;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hey");
+        System.out.print("Welcome to Coding Blocks");
+        System.out.println("bye");
+    }
+}
+
+
---
+++ Lec2/Variable.java
@@ -0,0 +1,22 @@
+package Lec2;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        
+
+        int a = 20;
+        int b = 10;
+
+        int sum = a+b;
+
+        System.out.println(sum);
+
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+    
+
+}
