241e98942215f04b8ea41d3c5e54437a10fbe61c
hansrajathole <hansrajathole1@gmail.com>
2026-01-21 19:02:07 +0530

    Add initial implementations of Pattern2, Pattern3, WhileDemo classes and Lec4.pdf with example outputs

---
+++ Lec4/Pattern3.java
@@ -0,0 +1,25 @@
+package Lec4;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        int n = 5;
+        int star = n;
+        int row = 1;
+
+        while (row<=n) {
+            
+            //Star
+            int i = 1;
+            while (i<=star) {
+                System.out.print("* ");
+                i++;
+            }
+
+            // Next line prep
+            star--;
+            row++;
+            System.out.println();
+
+        }
+    }
+}
---
+++ Lec4/WhileDemo.java
@@ -0,0 +1,18 @@
+
+
+package Lec4;
+
+public class WhileDemo {
+
+    public static void main(String[] args) {
+        
+        int n = 10;
+        int i = 1;
+
+        while (i<=5) {
+            System.out.print("* ");
+            i++;
+        }
+    }
+     
+}
