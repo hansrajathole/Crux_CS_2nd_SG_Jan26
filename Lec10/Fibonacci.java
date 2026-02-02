b02301d7197965dd4df42f10f05e7e3138257f2a
hansrajathole <hansrajathole1@gmail.com>
2026-02-02 20:24:10 +0530

    Maths

---
+++ Lec10/Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+
+        int n = 8;
+
+        for(int i = 1; i<=n; i++){
+            int next = a+b;
+            a = b;
+            b = next;
+        }
+       
+        System.out.println(a);
+    }
+}
---
+++ Lec10/Prime.java
@@ -0,0 +1,20 @@
+public class Prime {
+    public static void main(String[] args) {
+            int n = 11;
+            int count = 0;
+
+            for(int i = 2; i<=n-1;i++){
+                if(n%i == 0){
+                    count++;
+                    break;
+                }
+            }
+
+            if(count == 0){
+                System.out.println("Prime number");
+            }else {
+                System.out.println("not Prime");
+            }
+
+    }
+}
