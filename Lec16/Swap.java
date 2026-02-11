cc4f29cd4a21838279ec58fb01321d12e3625c5c
hansrajathole <hansrajathole1@gmail.com>
2026-02-11 21:43:11 +0530

    Swap Array

---
+++ Lec16/Swap.java
@@ -0,0 +1,17 @@
+public  class Swap {
+    public static void main(String [] args){
+        int[] arr = {10,20,30,40,50};
+        int[] other = {1,2,3,4,5};
+
+        Swap(arr, other);
+
+        System.out.println(arr[0]+"  "+arr[1]);
+
+    }
+
+    public static void Swap(int[] arr , int [] other ){
+        int[] temp = arr;
+        arr = other;
+        other = temp;
+    }
+}
