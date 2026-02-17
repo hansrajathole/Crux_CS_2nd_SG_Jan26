3eec9fcafa1c5286145d2c2d27881d93c1e7925d
hansrajathole <hansrajathole1@gmail.com>
2026-02-17 15:01:32 +0530

    Bubble sort

---
+++ Lec20/Bubble_sort.java
@@ -0,0 +1,30 @@
+public class Bubble_sort {
+
+    public static void main(String[] args) {
+        
+        int arr [] = { 5,2,6,4,7,1,8};
+        Print(arr);
+        System.out.println();
+        Sort(arr);
+        Print(arr);
+
+    }
+    public static void Print(int [] arr){
+        for(int i = 0 ; i<arr.length ; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void Sort(int [] arr){
+        for(int i = 0 ; i<arr.length-1; i++){
+            for(int j = 0 ; j<arr.length-i-1 ; j++){
+                if(arr[j]> arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+}
